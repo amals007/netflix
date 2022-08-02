6f1876107dca0859dd17be6eda68bb1c3f346253
Amal S <102399747+amals007@users.noreply.github.com>
2022-08-02 23:25:11 +0530

    css & movie categorie

---
+++ src/components/Banner.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import instance from "../instance";
+import requests from "../request";
+import "./Banner.css";
+function Banner() {
+  const [movie, setMovies] = useState([]);
+  async function fetchData() {
+    const request = await instance.get(requests.fetchNetflixOriginals);
+    setMovies(
+      request.data.results[
+        Math.floor(Math.random() * requst.data.results.length - 1)
+      ]
+    );
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log("Banner movie is ", movie);
+  return (
+    <header
+      className="header"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+                'https://image.tmdb.org/t/p/original/${movie.backdrop_path}'
+              )`,
+        backgroundPosition: "center center",
+        height: "800px",
+      }}
+    ></header>
+  );
+}
+export default Banner;
--- src/components/Row.js
+++ src/components/Row.js
@@ -4,7 +4,7 @@ import "./Row.css";
 
 const imageUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   async function fetchData() {
@@ -14,16 +14,16 @@ function Row({ title, fetchUrl }) {
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(movies);
+  
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${imageUrl}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${imageUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
           />
         ))}
